5b03205f4aec84588a79f249315c9b5d4b8707cf
nyeeldzn <nyeeldzn@gmail.com>
2021-06-24 22:23:53 -0300

    Testando a primeira classe de domain

--- src/main/java/com/sunonrails/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sunonrails/cursomc/resources/CategoriaResource.java
@@ -4,14 +4,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sunonrails.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	
 	@RequestMapping(method=RequestMethod .GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 }

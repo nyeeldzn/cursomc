0b953d096b2945f408d74f4d98dc60d2feca0469
nyeeldzn <nyeeldzn@gmail.com>
2021-06-27 22:19:09 -0300

    Criando operação de Instanciação

--- src/main/java/com/sunonrails/cursomc/CursoMcApplication.java
+++ src/main/java/com/sunonrails/cursomc/CursoMcApplication.java
@@ -1,13 +1,34 @@
 package com.sunonrails.cursomc;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import com.sunonrails.cursomc.domain.Categoria;
+import com.sunonrails.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}  
 
 }

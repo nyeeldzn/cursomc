b1a5acbc388f266e1c2bad3d224eb20538d03b71
nyeeldzn <nyeeldzn@gmail.com>
2021-06-30 10:07:05 -0300

    Implementando DELETE e Tratando Exceptions para Constraints

--- src/main/java/com/sunonrails/cursomc/services/CategoriaService.java
+++ src/main/java/com/sunonrails/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.sunonrails.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.sunonrails.cursomc.domain.Categoria;
 import com.sunonrails.cursomc.repositories.CategoriaRepository;
+import com.sunonrails.cursomc.services.exceptions.DataIntegrityException;
 import com.sunonrails.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -33,6 +35,15 @@ public class CategoriaService {
 		//method save atualiza se o id nao for nulo
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch (DataIntegrityViolationException ex) {
+					throw new DataIntegrityException("Não é possível deletar uma categoria com produtos");
+				}
 	}
 	
 }

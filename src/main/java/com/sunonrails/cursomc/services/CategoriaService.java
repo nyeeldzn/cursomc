8aab02a6e103d5e7952bf22da44432163734aaef
nyeeldzn <nyeeldzn@gmail.com>
2021-06-30 11:03:10 -0300

    Paginacao com parametros na requisição

--- src/main/java/com/sunonrails/cursomc/services/CategoriaService.java
+++ src/main/java/com/sunonrails/cursomc/services/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.sunonrails.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.sunonrails.cursomc.domain.Categoria;
@@ -25,6 +29,16 @@ public class CategoriaService {
 				"Objeto não encontrado! ID: " + id  + ", Tipo: " + Categoria.class.getName()));
 	
 	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		//method save insere se o id for nulo
